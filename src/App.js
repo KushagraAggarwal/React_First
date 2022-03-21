fefce76723f3a47499ae7bdfdb9e7cf3394e9bad
Kushagra <Kushagra.8oct@gmail.com>
2022-03-21 17:50:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import "./App.css";
+import {CardList} from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component"
+
+class App extends Component {
+  state = {
+    monsters: [],
+    searchField: "",
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  onHandleChange = (e)=> {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monster"
+          handleChange={this.onHandleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+import './index.css';
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+ReactDom.render(<App />, document.getElementById("root"));
